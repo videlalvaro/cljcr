f1d9432d7f65cfd10d0e367885b88489a86ba9ba
Alvaro Videla <videlalvaro@gmail.com>
2011-10-11 12:53:55 +0200

    modifies the dev dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 ;;      http://www.apache.org/licenses/LICENSE-2.0
 ;;
 ;;    Unless required by applicable law or agreed to in writing, software
-;;    distributed under the License is distributed on an "AS IS" BASIS,
+;;    distributed under the License is distributed on an "AS IS" BASIS
 ;;    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ;;    See the License for the specific language governing permissions and
 ;;    limitations under the License.
@@ -19,4 +19,5 @@
                      [org.clojure.contrib/except "1.3.0-SNAPSHOT"]
                      [javax.jcr/jcr "2.0"]]
   :dev-dependencies [[org.apache.jackrabbit/jackrabbit-core "2.2.9"]
+                     [org.apache.jackrabbit/jackrabbit-jcr-rmi "2.2.8"]
                      [org.slf4j/slf4j-log4j12 "1.5.8"]])
